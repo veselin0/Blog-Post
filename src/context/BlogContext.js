e71853b98e3c698fee705f8c95ac798600775462
Veselin <vesselin60@gmail.com>
2022-02-01 11:50:02 +0200

    Rendering a list of posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,8 +3,13 @@ import React, { createContext } from 'react';
 const BlogContext = createContext();
 
 export const BlogProvider = ({children}) => {
+    const blogPosts = [
+        {title: 'Blog Post #1'},
+        {title: 'Blog Post #2'}
+    ];
+
     return (
-        <BlogContext.Provider value={['Gocho!', 'Bocho', 'Docho']}>
+        <BlogContext.Provider value={blogPosts} >
             {children}
         </BlogContext.Provider>
     );
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,19 +1,25 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-	const value = useContext(BlogContext);
+  const blogPosts = useContext(BlogContext);
 
   return (
-    <View>
+    <View style={styles.text}>
       <Text>Index Screen</Text>
-      <Text>{value}</Text>
+      <FlatList data={blogPosts} keyExtractor={blogPost => blogPost.title} renderItem={({item}) => {
+		  return <Text>{item.title}</Text>;
+	  }} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	text: {
+		alignItems: 'center',
+	}
+});
 
 export default IndexScreen;
